f937049b188e151aca73d1ae3bb48b7455285693
EducationManMaster <71923384+EducationManMaster@users.noreply.github.com>
2022-03-23 08:38:00 +0100

    Update lf8_taskmanager_socket.py

--- python/lf8_taskmanager_socket.py
+++ python/lf8_taskmanager_socket.py
@@ -10,7 +10,7 @@ def getStats():
     ram = psutil.virtual_memory().percent
     temp = psutil.sensors_temperatures()
     disk = psutil.disk_usage('/').percent
-    network = "wlan0" #psutil.net_io_counters(pernic=True)
+    network = "eth0" #psutil.net_io_counters(pernic=True)
     ip = psutil.net_if_addrs()
     processes = os.popen("ps -e | wc -l").read().splitlines()[0]
     pid = os.popen("top -n 1 -b").read()
